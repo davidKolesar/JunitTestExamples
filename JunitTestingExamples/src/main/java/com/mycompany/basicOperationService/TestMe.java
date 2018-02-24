35de9611d09ae0cc0a3259528d3f1aa3b8fac609
David Kolesar <djkolesa@gmail.com>
2018-02-24 10:29:33 -0500

    David Kolesar has added random String generator

--- JunitTestingExamples/src/main/java/com/mycompany/basicOperationService/TestMe.java
+++ JunitTestingExamples/src/main/java/com/mycompany/basicOperationService/TestMe.java
@@ -1,6 +1,7 @@
 package com.mycompany.basicOperationService;
 
 import java.util.Random;
+import java.util.UUID;
 
 public class TestMe {
 	int max = 50;
@@ -13,8 +14,22 @@ public class TestMe {
 		//Adds 2 to a random number
 		addTwo(randomValue);
 		
+		//generatesRandomString
+		String randomData = generateRandomString();
+		
 		//reverses a String
-		reverseString(String original);
+		reverseString(randomData);
+		
+	}
+
+	private String generateRandomString() {
+			//changes a random UUID into a String
+		    String randomUuid = UUID.randomUUID().toString();
+	        
+		    //creates random string of characters
+		    String randomChars = Long.toHexString(Double.doubleToLongBits(Math.random()));
+		    
+			return randomChars + randomUuid;
 		
 	}
 
@@ -32,6 +47,5 @@ public class TestMe {
 			reversedOriginal = reversingOriginal;
 		}
 		return reversedOriginal;
-		
 	}
 }
